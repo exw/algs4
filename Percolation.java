e3f5b724de14cfc305ac2974393c6b2fe53dbd94
exw <exw@openmailbox.org>
2015-01-04 12:27:21 -0600

    Stats off by a bit, possible integer -> double issue

--- Percolation.java
+++ Percolation.java
@@ -1,4 +1,15 @@
 public class Percolation {
+    
+    /*
+     * Week 1
+     * 
+     * The model: We model a percolation system using an N-by-N grid of sites. Each site is either open or blocked. 
+     * A full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites. 
+     * We say the system percolates if there is a full site in the bottom row. 
+     * In other words, a system percolates if we fill all open sites connected to the top row and that process fills some open site on the bottom row. 
+     * 
+     * */
+    
     private int gridLen;
     private WeightedQuickUnionUF p;
     private int pSize;
@@ -13,7 +24,7 @@ public class Percolation {
     private boolean done;
     
     private int pIndex(int i, int j){
-        return (i-1)*gridLen + j;
+        return (i-1)*gridLen + j-1;
     }
     
     private boolean outOfRange(int i, int j){
@@ -24,19 +35,24 @@ public class Percolation {
         if(outOfRange(i,j)){
             throw new IndexOutOfBoundsException();
         }
+        return;
     }
     
     private int combineStatus(int l, int r, int u, int d){
         int statusCode = 1;
-        if (l == 4 || r == 4){
+        
+        if (l == 4 || r == 4 || u == 4 || d == 4){
             return 4;
         }
-        if (u == 2 || l == 2 || r == 2){
+        
+        if (u == 2 || l == 2 || r == 2 || d == 2){
             statusCode = 2;
         }
-        if (d == 3 || l == 3 || r == 3){
+        
+        if (d == 3 || l == 3 || r == 3 || d == 3){
             statusCode = statusCode + 2;
         }
+        
         return statusCode;
     }
         
@@ -45,15 +61,17 @@ public class Percolation {
             throw new java.lang.IllegalArgumentException();
         }
         gridLen = N;
-        pSize = N*N + 1;
+        pSize = N*N;
         p = new WeightedQuickUnionUF(pSize);
         //initialize with all sites blocked
         status = new int[pSize];
+        return;
     }
     
     public void open(int i, int j){
         checkRange(i,j);
         int target = pIndex(i,j);
+        
         int[] adjIndex = new int[4];
         int[] adjStatus = new int[4];
         
@@ -62,22 +80,28 @@ public class Percolation {
         // left is i,j-1
         if(!outOfRange(i,j-1)){
             adjIndex[0] = pIndex(i,j-1);
-            //adjStatus[0] = status[p.find(adjIndex[0])];
-            adjStatus[0] = status[adjIndex[0]];
+            adjStatus[0] = status[p.find(adjIndex[0])];
+            if(adjStatus[0] > 0){
+                p.union(target,adjIndex[0]);
+            }
         }
         
         // right is i,j+1    
         if(!outOfRange(i,j+1)){
             adjIndex[1] = pIndex(i,j+1);
-            //adjStatus[1] = status[p.find(adjIndex[1])];
-            adjStatus[1] = status[adjIndex[1]];
+            adjStatus[1] = status[p.find(adjIndex[1])];
+            if(adjStatus[1] > 0){
+                p.union(target,adjIndex[1]);
+            }
         }
         
         // up is i-1,j
         if(!outOfRange(i-1,j)){
             adjIndex[2] = pIndex(i-1,j);
-            //adjStatus[2] = status[p.find(adjIndex[2])];
-            adjStatus[2] = status[adjIndex[2]];
+            adjStatus[2] = status[p.find(adjIndex[2])];
+            if(adjStatus[2] > 0){
+                p.union(target,adjIndex[2]);
+            }
         }
         else{
             adjStatus[2] = 2;
@@ -86,8 +110,10 @@ public class Percolation {
         // down is i+1,j
         if(!outOfRange(i+1,j)){
             adjIndex[3] = pIndex(i+1,j);
-            //adjStatus[3] = status[p.find(adjIndex[3])];
-            adjStatus[3] = status[adjIndex[3]];
+            adjStatus[3] = status[p.find(adjIndex[3])];
+            if(adjStatus[3] > 0){
+                p.union(target,adjIndex[3]);
+            }
         }
         else{
             adjStatus[3] = 3;
@@ -102,28 +128,18 @@ public class Percolation {
         if (newStatus == 4){
             done = true;
         }
-        
-        
-        for(int k=0;k<4;k++){
-            if(adjStatus[k] > 0 && adjIndex[k] > 0){
-                status[p.find(adjIndex[k])] = newStatus;
-                p.union(target,adjIndex[k]);    
-            }
-        }
-        // set newStatus for all connected components
+   
         status[p.find(target)] = newStatus;
-        status[target] = newStatus;
-        
         return;
     }
     public boolean isOpen(int i, int j){
         checkRange(i,j);
-        return (status[pIndex(i,j)] > 0);
+        return (status[p.find(pIndex(i,j))] > 0);
         
     }
     public boolean isFull(int i, int j){
         checkRange(i,j);
-        return (status[p.find(pIndex(i,j))] % 2 == 0 && status[pIndex(i,j)] > 0);
+        return (status[p.find(pIndex(i,j))] % 2 == 0 && status[p.find(pIndex(i,j))] > 0);
     }
     
     public boolean percolates(){
@@ -136,14 +152,24 @@ public class Percolation {
     public static void main(String[] args) {
         Percolation q = new Percolation(5);
         q.open(1,2);
+        if(q.isOpen(1,2)){
+            System.out.println("Open isOpen");
+        }
         q.open(5,5);
         q.open(2,2);
         q.open(3,2);
         q.open(4,2);
+        q.open(4,1);
+        if(q.isFull(4,1)){
+            System.out.println("isFull working");
+        }
         q.open(5,3);
         q.open(4,3);
         if(q.isFull(5,5)){
             System.out.println("Backwash issue");
+        }
+        if(q.percolates()){
+            System.out.println("Percolates");
         }
         return;
 
---
+++ PercolationStats.java
@@ -0,0 +1,86 @@
+public class PercolationStats {
+    
+    /*
+     * The problem. In a famous scientific problem, researchers are interested in the following question: 
+     * if sites are independently set to be open with probability p (and therefore blocked with probability 1 ? p), 
+     * what is the probability that the system percolates? 
+     * When p equals 0, the system does not percolate; when p equals 1, the system percolates. 
+     * The plots below show the site vacancy probability p versus the percolation probability 
+     * for 20-by-20 random grid (left) and 100-by-100 random grid (right). 
+     * 
+     * 
+     * */
+    
+    
+    private int[] results;
+    private int times;
+    private int pSize;
+    
+    private boolean[] reset(boolean b[]){
+        for(int i=0;i<pSize;i++){
+            b[i] = false;
+        }
+        return b;
+    }
+    
+    public PercolationStats(int N, int T){
+        // perform T independent experiments on an N-by-N grid
+        if (N <= 0 || T <= 0){
+            throw new java.lang.IllegalArgumentException("Negative or zero input not allowed");
+        }
+        
+        
+        results = new int[T];
+        times = T;
+        pSize = N*N;
+        boolean[] opened = new boolean[pSize];
+        
+        for(int k=0;k<T;k++){
+            System.out.println("Iteration: " + String.valueOf(k));
+            Percolation p = new Percolation(N);
+            reset(opened);
+            int result = 0;
+            while(!p.percolates()){
+                int i = StdRandom.uniform(1,N+1);
+                int j = StdRandom.uniform(1,N+1);
+                int target = (i-1)*N + j - 1;
+                
+                if(!opened[target]){
+                    p.open(i,j);
+                    result = result + 1;
+                    opened[target] = true;
+                }
+            }
+            System.out.println("Percolates at " + String.valueOf(result));
+            results[k] = result;
+        }
+    }
+    
+    public double mean(){
+        // sample mean of percolation threshold
+        return StdStats.mean(results)/((double) pSize);
+    }
+    
+    public double stddev(){
+        // sample standard deviation of percolation threshold
+        return StdStats.stddev(results)/((double) pSize);
+    }
+    
+    public double confidenceLo(){
+        // low  endpoint of 95% confidence interval
+        return (this.mean() - 1.96*this.stddev() / Math.sqrt(times));
+    }
+    
+    public double confidenceHi(){
+        // high endpoint of 95% confidence interval
+        return (this.mean() + 1.96*this.stddev() / Math.sqrt(times));
+    }
+    
+    public static void main(String[] args){
+        // test client (described below)
+        PercolationStats ps = new PercolationStats(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+        System.out.println("mean                    = " + String.valueOf(ps.mean()));
+        System.out.println("stddev                  = " + String.valueOf(ps.stddev()));
+        System.out.println("95% confidence interval = " + String.valueOf(ps.confidenceLo()) + ", " + String.valueOf(ps.confidenceHi()));
+    }
+}
